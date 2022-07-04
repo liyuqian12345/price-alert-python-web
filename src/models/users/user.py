12cb2123859810d621851c4809b35113f5a5430a
liyuqian12345 <99516805+liyuqian12345@users.noreply.github.com>
2022-07-04 16:08:14 -0400

    Update user.py

--- src/models/users/user.py
+++ src/models/users/user.py
@@ -61,7 +61,7 @@ class User(object):
         return True
 
     def save_to_db(self):
-        Database.insert("users", self.json())
+        Database.insert_one("users", self.json())
 
     def json(self):
         return {
